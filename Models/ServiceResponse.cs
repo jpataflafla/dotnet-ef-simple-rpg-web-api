13a163a4f3c855b585b91c3649ed16681717ac32
jpataflafla <jakub.kozlowski.mail@gmail.com>
2024-04-06 19:46:15 +0200

    Update ServiceResponse.cs

--- Models/ServiceResponse.cs
+++ Models/ServiceResponse.cs
@@ -5,4 +5,5 @@ public class ServiceResponse<T>
     public T? Data { get; set; }
     public bool Success { get; set; } = true;
     public string Message { get; set; } = string.Empty;
+    public static ServiceResponse<T> Fail(string message) => new ServiceResponse<T> { Success = false, Message = message };
 }
